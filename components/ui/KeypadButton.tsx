2a632752d8cfab5a4af0b437d7d680ec2c2fd1ed
Johan Ronsse <johan.ronsse@gmail.com>
2025-06-18 19:58:04 -0600

    Disabled button capabilities & tap

--- components/ui/KeypadButton.tsx
+++ components/ui/KeypadButton.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Pressable, Text } from 'react-native';
+import { Pressable, Text, View } from 'react-native';
 import Animated, { useSharedValue, useAnimatedStyle, withSpring } from 'react-native-reanimated';
 import { cn } from '~/lib/utils';
 
@@ -8,18 +8,36 @@ interface KeypadButtonProps {
   onPress: () => void;
   isIcon?: boolean;
   IconComponent?: React.ReactNode;
+  disabled?: boolean;
 }
 
-const KeypadButton: React.FC<KeypadButtonProps> = ({ label, onPress, isIcon = false, IconComponent }) => {
-
+const KeypadButton: React.FC<KeypadButtonProps> = ({ 
+  label, 
+  onPress, 
+  isIcon = false, 
+  IconComponent,
+  disabled = false 
+}) => {
   return (
     <Pressable
-    onPressOut={() => {
-      onPress();
-    }}
-      className='flex-1 bg-white rounded-2xl h-14 items-center justify-center active:bg-gray-200'
+      onPressOut={() => {
+        if (!disabled) {
+          onPress();
+        }
+      }}
+      className={cn(
+        'flex-1 bg-white rounded-2xl h-14 items-center justify-center active:bg-gray-200',
+        disabled && 'opacity-50'
+      )}
+      disabled={disabled}
     >
-      {isIcon && IconComponent ? IconComponent : <Text className='text-xl font-medium'>{label}</Text>}
+      {isIcon && IconComponent ? (
+        <View className={cn(disabled && 'opacity-50')}>
+          {IconComponent}
+        </View>
+      ) : (
+        <Text className='text-xl font-medium'>{label}</Text>
+      )}
     </Pressable>
   );
 };
